cb3d40b15333092b36c3316869b0e697e623d17b
Noh Jihyun <blue900823@gmail.com>
2025-07-20 22:24:52 +0900

    Initial commit

---
+++ src/main/java/com/nakshi/fishingbackend/ReactRestApIbApplication.java
@@ -0,0 +1,13 @@
+package com.nakshi.fishingbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactRestApIbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReactRestApIbApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nakshi/fishingbackend/ReactRestApIbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nakshi.fishingbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactRestApIbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
